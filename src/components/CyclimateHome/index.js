51ae055f7c560f69645beb70c122ecb28b03abf9
Santiago <salviega6@gmail.com>
2022-11-18 12:33:02 -0500

    [STYLES] Was added background photo

--- src/components/CyclimateHome/index.js
+++ src/components/CyclimateHome/index.js
@@ -1,19 +1,20 @@
 import "./CyclimateHome.scss";
 import React from "react";
 import logo from "../../assets/images/logo-Cyclimate.png";
+import bussinesModel from "../../assets/images/bussines-model.png";
 import { CyclimateEvent } from "./CyclimateEvent";
 import { CyclimateEvents } from "./CyclimateEvents";
-import { CyclimateLoading } from "../../shared/CyclimateLoading";
 
 export function CyclimateHome({ items: events, loading, error }) {
   return (
     <div className="home">
       <div className="home__start_page">
-        <img src={logo} alt="logo" className="home__logo" />
         <h1 className="home__title">Cyclimate</h1>
+        <img src={logo} alt="logo" className="home__logo" />
         <h2 className="home__description">
           Fight global warming while pedaling.
         </h2>
+        <img src={bussinesModel} alt="logo" className="home__bussines" />
       </div>
       <CyclimateEvents>
         {events?.map((event, index) => (
