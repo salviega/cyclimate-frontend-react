3c9e2e8cf88c0873dd1173874fea56c04967072b
salviega <salviega6@gmail.com>
2022-12-27 15:59:31 -0500

    [CHORE] Was added feating on webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,12 @@
 const path = require('path')
-// const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
-  performance: {
-    hints: false
-  },
   entry: './src/index.js',
   output: {
-    filename: '[name].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
@@ -53,34 +49,27 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|svg|ico)$/,
-        type: 'asset/resource'
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'assets/[hash].[ext]' }
+          }
+        ]
       }
       // TODO: Update fonts
-
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      filename: './[name].html',
       template: './public/index.html',
+      filename: './index.html',
       favicon: './public/favicon.ico',
       manifest: './public/manifest.json'
     }),
     new MiniCssExtractPlugin({
       filename: './[name].css'
     })
-    // new CopyPlugin({
-    //   patterns: [
-    //     {
-    //       from: path.resolve(__dirname, 'src', 'assets/images'),
-    //       to: 'assets/images'
-    //     }
-    //   ]
-    // })
-
-    // TODO: Update environment variables
-    // new Dotenv(),
   ],
   devServer: {
     static: path.join(__dirname, 'dist'),
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 // const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'production',
-  performance: {
-    hints: false
-  },
+  // performance: {
+  //   hints: false
+  // },
   entry: './src/index.js',
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   resolve: {
     // TODO: Generate alias
@@ -55,23 +58,27 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|svg|ico)$/,
-        type: 'asset/resource'
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'assets/[hash].[ext]' }
+          }
+        ]
       }
       // TODO: Update fonts
-
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      filename: './[name].html',
       template: './public/index.html',
+      filename: './index.html',
       favicon: './public/favicon.ico',
       manifest: './public/manifest.json'
     }),
     new MiniCssExtractPlugin({
       filename: './[name].css'
-    })
+    }),
     // new CopyPlugin({
     //   patterns: [
     //     {
@@ -80,9 +87,8 @@ module.exports = {
     //     }
     //   ]
     // })
-
-    // TODO: Update environment variables
-    // new Dotenv(),
+    new CleanWebpackPlugin(),
+    new Dotenv()
   ],
   optimization: {
     minimize: true,
